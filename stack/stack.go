6708b07d58d0b0018a24853f38141d7f5a8a8eb3
Sam <copperboat@hey.com>
2024-05-05 08:19:09 -0500

    Stack: use generics

--- stack/stack.go
+++ stack/stack.go
@@ -5,19 +5,19 @@ import "sync"
 // A stack is a first-in-last-out (FILO) data structure: the last element pushed is the first
 // element popped. The zero value is an empty stack and ready to use, and stacks are safe for
 // concurrent use.
-type Stack struct {
-	top   *node
+type Stack[T any] struct {
+	top   *node[T]
 	mutex sync.Mutex
 }
 
 // A node is a single element in a stack.
-type node struct {
-	v    any
-	next *node
+type node[T any] struct {
+	v    T
+	next *node[T]
 }
 
 // Push adds a value to the top of the stack.
-func (s *Stack) Push(v any) {
+func (s *Stack[T]) Push(v T) {
 	if s == nil {
 		return
 	}
@@ -25,23 +25,24 @@ func (s *Stack) Push(v any) {
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
 
-	s.top = &node{
+	s.top = &node[T]{
 		v:    v,
 		next: s.top,
 	}
 }
 
-// Pop removes and returns the value at the top of the stack.
-func (s *Stack) Pop() any {
+// Pop removes and returns the value at the top of the stack. If the stack is empty, this returns
+// the zero value of the stack's type.
+func (s *Stack[T]) Pop() (t T) {
 	if s == nil {
-		return nil
+		return
 	}
 
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
 
 	if s.top == nil {
-		return nil
+		return
 	}
 
 	v := s.top.v
@@ -50,24 +51,25 @@ func (s *Stack) Pop() any {
 	return v
 }
 
-// Peek returns the value at the top of the stack without removing it.
-func (s *Stack) Peek() any {
+// Peek returns the value at the top of the stack without removing it. If the stack is empty, this
+// returns the zero value of the stack's type.
+func (s *Stack[T]) Peek() (t T) {
 	if s == nil {
-		return nil
+		return
 	}
 
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
 
 	if s.top == nil {
-		return nil
+		return
 	}
 
 	return s.top.v
 }
 
 // Empty returns true if the stack is empty.
-func (s *Stack) Empty() bool {
+func (s *Stack[T]) Empty() bool {
 	if s == nil {
 		return true
 	}
@@ -79,7 +81,7 @@ func (s *Stack) Empty() bool {
 }
 
 // Size returns the number of elements in the stack.
-func (s *Stack) Size() int {
+func (s *Stack[T]) Size() int {
 	if s == nil {
 		return 0
 	}
@@ -96,7 +98,7 @@ func (s *Stack) Size() int {
 }
 
 // Clear removes all elements from the stack.
-func (s *Stack) Clear() {
+func (s *Stack[T]) Clear() {
 	if s == nil {
 		return
 	}
