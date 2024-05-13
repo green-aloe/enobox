76df14f549635c716d3842055ec97a0c0120d6a9
Sam <copperboat@hey.com>
2024-05-13 08:49:53 -0500

    Note: move to separate package

---
+++ note/note.go
@@ -0,0 +1,26 @@
+package note
+
+const (
+	Sharp = "♯"
+	Flat  = "♭"
+
+	C      Note = "C"
+	CSharp Note = C + Sharp
+	DFlat  Note = D + Flat
+	D      Note = "D"
+	DSharp Note = D + Sharp
+	EFlat  Note = E + Flat
+	E      Note = "E"
+	F      Note = "F"
+	FSharp Note = F + Sharp
+	GFlat  Note = G + Flat
+	G      Note = "G"
+	GSharp Note = G + Sharp
+	AFlat  Note = A + Flat
+	A      Note = "A"
+	ASharp Note = A + Sharp
+	BFlat  Note = B + Flat
+	B      Note = "B"
+)
+
+type Note string
--- note/note_test.go
+++ note/note_test.go
@@ -1,7 +1,6 @@
-package tone
+package note
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -54,42 +53,4 @@ func Test_Notes(t *testing.T) {
 		require.Equal(t, Note("B"), B)
 		require.IsType(t, note, B)
 	})
-}
-
-// Test_NewToneFrom tests that NewToneFrom returns a tone that has been initialized with the correct
-// fundamental frequency for various notes and octaves.
-func Test_NewToneFrom(t *testing.T) {
-
-	t.Run("invalid note", func(t *testing.T) {
-		tone := NewToneFrom(Note("H"), 5)
-		require.Zero(t, tone)
-
-		tone = NewToneFrom(Note(C+"b"), 5)
-		require.Zero(t, tone)
-	})
-
-	t.Run("invalid octave", func(t *testing.T) {
-		tone := NewToneFrom(C, -2)
-		require.Zero(t, tone)
-
-		tone = NewToneFrom(C, 11)
-		require.Zero(t, tone)
-	})
-
-	for _, note := range []Note{C, CSharp, DFlat, D, DSharp, EFlat, E, F, FSharp, GFlat, G, GSharp, AFlat, A, ASharp, BFlat, B} {
-		for octave := -1; octave <= 10; octave++ {
-			t.Run(fmt.Sprintf("%v%v", note, octave), func(t *testing.T) {
-				tone := NewToneFrom(note, octave)
-				require.NotEmpty(t, tone)
-				require.IsType(t, Tone{}, tone)
-				require.Greater(t, tone.Frequency, float32(8))
-				require.Equal(t, noteFreqs[note][octave], tone.Frequency)
-				require.Zero(t, tone.Gain)
-				require.Len(t, tone.HarmonicGains, NumHarmGains)
-				for _, gain := range tone.HarmonicGains {
-					require.Equal(t, float32(0), gain)
-				}
-			})
-		}
-	}
 }
