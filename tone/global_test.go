e853d21c77eaf539e4ce663a7d0721ba00c4d69c
Sam <copperboat@hey.com>
2024-07-25 07:58:38 -0500

    Tone: add tests for NumHarmGains

--- tone/global_test.go
+++ tone/global_test.go
@@ -20,3 +20,55 @@ func Test_init(t *testing.T) {
 	ctx := context.NewContext()
 	require.Equal(t, 20, NumHarmGains(ctx))
 }
+
+// Test_NumHarmGains tests that NumHarmGains returns the correct number of harmonic gains in a tone
+// for the given context.
+func Test_NumHarmGains(t *testing.T) {
+	t.Run("nil context", func(t *testing.T) {
+		require.Zero(t, NumHarmGains(nil))
+	})
+
+	t.Run("no value set", func(t *testing.T) {
+		ctx := context.NewTestContext()
+		require.Zero(t, NumHarmGains(ctx))
+	})
+
+	t.Run("non-integer value", func(t *testing.T) {
+		for _, v := range []any{uint64(20), true, "20", 20.0, int8(20), byte(20), rune(20)} {
+			ctx := context.NewContextWith(context.ContextOptions{
+				Decorators: []context.Decorator{
+					func(ctx context.Context) context.Context {
+						return ctx.WithValue(numHarmGainsKey, v)
+					},
+				},
+			})
+			require.Zero(t, NumHarmGains(ctx))
+		}
+	})
+
+	t.Run("invalid values", func(t *testing.T) {
+		for _, n := range []int{0, -1, -20} {
+			ctx := context.NewContextWith(context.ContextOptions{
+				Decorators: []context.Decorator{
+					func(ctx context.Context) context.Context {
+						return ctx.WithValue(numHarmGainsKey, n)
+					},
+				},
+			})
+			require.Zero(t, NumHarmGains(ctx))
+		}
+	})
+
+	t.Run("valid values", func(t *testing.T) {
+		for n := range 1_000 {
+			ctx := context.NewContextWith(context.ContextOptions{
+				Decorators: []context.Decorator{
+					func(ctx context.Context) context.Context {
+						return ctx.WithValue(numHarmGainsKey, n+1)
+					},
+				},
+			})
+			require.Equal(t, n+1, NumHarmGains(ctx))
+		}
+	})
+}
