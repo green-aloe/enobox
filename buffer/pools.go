c53efa7f4695f470ec27c66a7b3a92d41a984519
Sam <copperboat@hey.com>
2024-07-28 15:05:58 -0500

    Buffer: pools initial commit

---
+++ buffer/pools.go
@@ -0,0 +1,63 @@
+package buffer
+
+import (
+	"sync"
+
+	"github.com/green-aloe/enobox/context"
+	"github.com/green-aloe/enobox/tone"
+	"github.com/green-aloe/utilities/pool"
+)
+
+var (
+	// We must use a custom pool implementation instead of the more standard sync.Pool because the
+	// latter can drop buffers at any time, which isn't good for us because we might need to close
+	// the buffer before releasing it (so that non-go garbage collectors can also release their
+	// references to the memory).
+	bufferPools      = make(map[bufferPoolsKey]*pool.Pool[Buffer])
+	bufferPoolsMutex sync.Mutex
+	bufferPoolKey    bufferPoolCtxKey
+)
+
+type bufferPoolsKey struct {
+	sampleRate   int
+	numHarmGains int
+}
+
+type bufferPoolCtxKey struct{}
+
+func init() {
+	// Add a context decorator that sets a buffer pool in each new context depending on the sample
+	// rate and number of harmonic gains in a tone configured for the context. (The decorator for
+	// setting the number of harmonic gains is guaranteed to be added to the pipeline before this
+	// one because this one calls the package for the other one, which means that package will
+	// finish its initialization first.)
+	context.AddDecorator(func(ctx context.Context) context.Context {
+		bufferPoolsMutex.Lock()
+		defer bufferPoolsMutex.Unlock()
+
+		key := bufferPoolsKey{
+			sampleRate:   ctx.SampleRate(),
+			numHarmGains: tone.NumHarmGains(ctx),
+		}
+
+		bufferPool, ok := bufferPools[key]
+		if !ok || bufferPool == nil {
+			bufferPool = &pool.Pool[Buffer]{
+				NewItem: func() Buffer { return NewBuffer(ctx) },
+			}
+			bufferPools[key] = bufferPool
+		}
+
+		return ctx.WithValue(bufferPoolKey, bufferPool)
+	})
+}
+
+func BufferPool(ctx context.Context) *pool.Pool[Buffer] {
+	if v := ctx.Value(bufferPoolKey); v != nil {
+		if p, ok := v.(*pool.Pool[Buffer]); ok {
+			return p
+		}
+	}
+
+	return nil
+}
